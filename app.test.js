b0410b5a2bdbb09b6e7da71eb23c615f71489361
amriteshx1 <amriteshx18@gmail.com>
2025-02-22 18:28:09 +0530

    Add unit tests for Gameboard class

--- app.test.js
+++ app.test.js
@@ -1,17 +1,44 @@
-const Ship = require('./structure');
+const {Ship, Gameboard} = require('./structure');
 
 
-describe('test the ship object', () => {
+describe('test the Ship class', () => {
     test('ship', () => {
         const s = new Ship(3);
         expect(s.length).toBe(3);
+
         s.hit();
         expect(s.hits).toBe(1);
+
         s.hit();
         expect(s.hits).toBe(2);
         expect(s.isSunk()).toBe(false);
+
         s.hit();
         expect(s.hits).toBe(3);
         expect(s.isSunk()).toBe(true);
-    })
-})
+    });
+});
+
+describe('test the Gameboard class', () => {
+    test('gameboard', () => {
+        const board = new Gameboard();
+        const ship = new Ship(3);
+        board.placeShip(ship, [[2,1], [2,2], [2,3]]);
+        expect(board.grid[2][2]).toBe(ship);
+
+        board.receiveAttack(2,1);
+        expect(ship.hits).toBe(1);
+
+        board.receiveAttack(2,4);
+        expect(board.misAttack).toContainEqual([2, 4]);
+
+        board.receiveAttack(2,2);
+        expect(board.allSunk()).toBe(false);
+
+        board.receiveAttack(2,3);
+        expect(board.allSunk()).toBe(true);
+    });
+});
+
+
+
