81ff8756449e446772c9447219222fd5c6e4e898
amriteshx1 <amriteshx18@gmail.com>
2025-02-22 19:03:45 +0530

    Create Player class with a gameboard

--- structure.js
+++ structure.js
@@ -2,40 +2,47 @@ class Ship{
     constructor(length){
         this.length = length;
         this.hits = 0;
-    }
+    };
 
     hit(){
         this.hits ++;
-    }
+    };
 
     isSunk(){
         return this.hits >= this.length;
-    }
-}
+    };
+};
 
 class Gameboard{
     constructor (){
         this.grid = Array(10).fill(null).map(() => Array(10).fill(null));
         this.ships = [];
         this.misAttack = [];
-    }
+    };
 
     placeShip(ship, coordinates){
         coordinates.forEach(([x, y]) => this.grid[x][y] = ship);
         this.ships.push(ship);
-    }
+    };
 
     receiveAttack(x, y){
         if(this.grid[x][y]){
             this.grid[x][y].hit();
         }else{
             this.misAttack.push([x, y]);
-        }
-    }
+        };
+    };
 
     allSunk(){
         return this.ships.every(ship => ship.isSunk());
-    }
-}
+    };
+};
 
-module.exports = {Ship, Gameboard};
+class Player{
+    constructor(isComputer = false){
+        this.isComputer = isComputer;
+        this.board = new Gameboard();
+    };
+};
+
+module.exports = {Ship, Gameboard, Player};
