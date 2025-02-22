b79682c42397a17741d16cf826a8b9fa095f10e6
amriteshx1 <amriteshx18@gmail.com>
2025-02-22 18:27:30 +0530

    Implement Gameboard class with ship placement, attack handling, and sinking detection

--- structure.js
+++ structure.js
@@ -13,4 +13,29 @@ class Ship{
     }
 }
 
-module.exports = Ship;
+class Gameboard{
+    constructor (){
+        this.grid = Array(10).fill(null).map(() => Array(10).fill(null));
+        this.ships = [];
+        this.misAttack = [];
+    }
+
+    placeShip(ship, coordinates){
+        coordinates.forEach(([x, y]) => this.grid[x][y] = ship);
+        this.ships.push(ship);
+    }
+
+    receiveAttack(x, y){
+        if(this.grid[x][y]){
+            this.grid[x][y].hit();
+        }else{
+            this.misAttack.push([x, y]);
+        }
+    }
+
+    allSunk(){
+        return this.ships.every(ship => ship.isSunk());
+    }
+}
+
+module.exports = {Ship, Gameboard};
