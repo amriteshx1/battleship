469d3f5f6f00f0f395f7e686f6a271d317145272
amriteshx1 <amriteshx18@gmail.com>
2025-02-23 19:44:12 +0530

    Add attack functionality to player 2's board with event listener on squares

--- dom.js
+++ dom.js
@@ -21,29 +21,55 @@ for(let i = 0; i < 100; i++){
 }
 
 
+let player1, player2;
+let currentPlayer;
+
 newGame.addEventListener('click', (event) => {
     event.preventDefault();
 
-    const player1 = new Player();
-    const player2 = new Player(true);
+    player1 = new Player();
+    player2 = new Player(true);
 
     const ship1 = new Ship(4);
     const ship2 = new Ship(3);
     const ship3 = new Ship(2);
+    const ship4 = new Ship(3);
+    const ship5 = new Ship(1);
+    const ship6 = new Ship(2);
 
     player1.board.placeShip(ship1, [[2,3], [2,4], [2,5], [2,6]]);
     player1.board.placeShip(ship3, [[7,7], [7,8]]);
     player1.board.placeShip(ship2, [[4,3],[5,3],[6,3]]);
+    player1.board.placeShip(ship4, [[9,3], [9,4], [9,5]]);
+    player1.board.placeShip(ship5, [[0,7]]);
+    player1.board.placeShip(ship6, [[5,8], [5,9]]);
 
-    player2.board.placeShip(ship2, [[8,6], [8,7], [8,8]]);
-    player2.board.placeShip(ship1, [[3,5], [4,5], [5,5], [6,5]]);
+    player2.board.placeShip(ship2, [[8,3], [8,4], [8,5]]);
+    player2.board.placeShip(ship1, [[2,7], [3,7], [4,7], [5,7]]);
     player2.board.placeShip(ship3, [[5,2], [5,3]]);
+    player2.board.placeShip(ship4, [[0,4], [1,4], [2,4]]);
+    player2.board.placeShip(ship5, [[9,8]]);
+    player2.board.placeShip(ship6, [[7,7], [7,8]]);
     
     showShip(player1.board.grid, player2.board.grid, square1List, square2List);
 
+    square2List.forEach((square, index) => {
+        square.addEventListener('click', function handleAttack() {
+            const row = Math.floor(index / 10);
+            const col = index % 10;
+            
+            player2.board.receiveAttack(row, col);
+
+            if(square.dataset.visited)return;
+            square.dataset.visited = 'true';
+    
+            if(square.dataset.ship){
+                square.textContent = "💥";
+            }else{
+                square.textContent = "❌";
+            }
+        })
+    })
 });
-
-
-
 
 
